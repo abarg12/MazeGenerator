b436cb17214904c7cdfc895ef05921816d304e6c
Aidan Barg <aidan.barg@gmail.com>
2023-01-01 21:44:03 -0500

    initialize blank grid

--- maze.py
+++ maze.py
@@ -1,3 +1,33 @@
 import pygame
 
-print("todo: make a maze")
+
+rows, cols = 16, 16
+
+
+class Cell:
+    def __init__(self):
+        self.hollow = False
+
+
+def main():     
+    grid = make_map(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            print(grid[i][j].hollow)
+    # cellStack = []
+
+
+def make_map(r, c):
+    mapArr = []
+
+    for i in range(r):
+        col = []
+        for j in range(c):
+            col.append(Cell())
+        mapArr.append(col)
+
+    return mapArr
+
+
+if __name__ == "__main__":
+    main()
